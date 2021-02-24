ad3af75620a59087dbbfe2f8e1b53f6eb82b90ab
acdra1n <r@yaboi.wtf>
2021-02-25 00:42:18 +0400

    fix mistake

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ const api_base = require('./lib/api/base');
 app.use('/assets', express.static(path.join(__dirname, "assets")));
 
 // Create pages
-app.use('/', api_base.createPugStaticHandler("./ui/pages/index.pug"));
+app.get('/', api_base.createPugStaticHandler("./ui/pages/index.pug"));
 
 app.listen(config.port, ()=>console.log("Bruhco.in service up and running!"));
